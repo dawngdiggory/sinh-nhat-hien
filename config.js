ff7262e6083b50d3707be59cff269aa1033c3c61
dawngdiggory <137990191+dawngdiggory@users.noreply.github.com>
2023-06-29 21:48:47 +0700

    Add files via upload
    keke

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://gifft.me/d#a4niVh4xazdf96seoykZ';
+const eventName = 'Happy birthday!';
+const titleCard = 'Pour vous ';
+const contentCard = 'Ngày đặc biệt của em nhưng không còn anh ở bên,nhưng thôi khsao! Mừng em thêm tuổi mới,chúng ta không liên lạc một thời gian dài nhưng anh vẫn muốn gửi lời chúc sinh nhật đến em.. Chúc em công việc suôn sẻ, bớt đi áp lực và mệt mỏi.Mong rằng em sẽ ngủ sớm kh còn thức khuya nũa, cũng ít đau buồn vì cuộc sống. Tương lai như thế nào, phải bước tiếp mới biết được. Nhưng trước hết, hãy trở thành người mà em mong muốn, làm những điều mà bản thân yêu thích. Nghiêm khắc với bản thân, khoan dung với người khác..Mong rằng em sẽ luôn tươi cười và hạnh phúc. Chúc em ngày sinh nhật ý nghĩa...💘..nhớ ấn vào tấm ảnh nha có bất ngờ đó :3';
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
